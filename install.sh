77f6005164423fc2452988bd9d3387f561e96597
Vladimir P Ramik <vladimir.ramik@nytimes.com>
2017-05-17 12:07:06 -0400

    wip

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Test bootstrap
+# Install python ansible dependency
+echo "Testing install.sh script output for sometests."
+# if command -v yum list | grep libselinux-python >/dev/null 2>&1; then
+#     echo "Python dependency installed."
+# else
+#     sudo yum -y install libselinux-python
+# fi
