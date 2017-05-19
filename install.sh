295281e7624aca88d9c181c48ee6bfec8290441c
Vladimir P Ramik <vladimir.ramik@nytimes.com>
2017-05-19 10:37:11 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
-# Test bootstrap
-# Install python ansible dependency
-echo "Testing install.sh script output for sometests."
-# if command -v yum list | grep libselinux-python >/dev/null 2>&1; then
-#     echo "Python dependency installed."
-# else
-#     sudo yum -y install libselinux-python
-# fi
+sudo yum -y update
+
+# To speed up iterations
+sudo yum install -y deltarpm
+
+sudo yum install -y ca-certificates git nodejs build-essential
+sudo yum install -y gcc-c++ make
+
+sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+sudo yum install -y nodejs
+sudo npm install pm2@latest -g
+
+sudo yum -y install epel-release
+sudo yum -y install redis
+
+# systemctl start redis.service
+# systemctl enable redis.service
+
+# sudo pm2 stop /opt/app/app.js
+# sudo export HOME=/root
+
+cd /opt/app
+sudo npm install
+
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/app
+
+sudo npm start
+
+
+# sudo pm2 start /opt/app/app.js
+# sudo pm2 restart /opt/app/app.js
